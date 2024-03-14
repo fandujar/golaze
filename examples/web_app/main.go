0475ed80cd3235e9d494967d8108c7c671b186bf
Filipe Andujar <filipeandujar@gmail.com>
2024-03-14 18:25:38 -0300

    feat: add examples

---
+++ examples/web_app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/fandujar/golaze"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+	router := golaze.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		golaze.JSONResponse(w, map[string]string{"message": "Hello, World!"}, http.StatusOK)
+	})
+
+	webapp := golaze.NewWebApp(
+		&golaze.WebAppConfig{
+			Port:   "8080",
+			Router: router,
+		},
+	)
+
+	app := golaze.NewApp(
+		&golaze.AppConfig{
+			LogLevel: zerolog.DebugLevel,
+			WebApp:   webapp,
+		},
+	)
+
+	app.Run()
+
+}
